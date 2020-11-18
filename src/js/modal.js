eb648dcbd087c71731b8a32102012bd513452c99
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-11-18 13:46:16 +0200

    fixed a lot of bugs

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,26 +1,21 @@
 //Импорты
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
-import modalCardTpl from '../templates/modal-card.hbs'; 
+import modalCardTpl from '../templates/modal-card.hbs';
 
-
-
-const filmContainerEl=document.querySelector('.film-container')
+const filmContainerEl = document.querySelector('.film-container');
 
 filmContainerEl.addEventListener('click', onFilmCardClick);
-console.log('MODAL')
-
-
+console.log('MODAL');
 
 function onFilmCardClick(e) {
-    console.log('CLiCK');
-    if (e.target.nodeName !== 'IMG') {
+  console.log('CLiCK');
+  if (e.target.nodeName !== 'IMG') {
     return;
   }
 
-    const filmCard = modalCardTpl();
-    const instance = basicLightbox.create(filmCard);
-   
+  const filmCard = modalCardTpl();
+  const instance = basicLightbox.create(filmCard);
+
   instance.show();
 }
-
