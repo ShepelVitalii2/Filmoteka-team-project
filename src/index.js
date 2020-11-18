3af2d0e6c9002717e944b2b1b0b02e9b379c0ba1
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-11-18 19:39:51 +0200

    function spinner start & stop added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './sass/main.scss';
+import { startSpinner, stopSpinner } from './js/spinner';
 import {
   fetchTopWeekMovie,
   fetchMovieByKeyWord,
   fetchMovieFullInfo,
 } from './js/apiService';
+
 import './js/modal';
+import './js/spinner';
 
 // fetchTopWeekMovie();
 // fetchMovieByKeyWord();
 // fetchMovieFullInfo(2);
+
+// startSpinner();
+// stopSpinner();
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,2 +1,36 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+
+const pagination = new Pagination('pagination');
+
+const container = document.getElementById('pagination');
+const pagination = new Pagination(container);
+
+const options = {
+  totalItems: 500,
+  itemsPerPage: 20,
+  visiblePages: 10,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+const pagination = new Pagination('pagination', options);
