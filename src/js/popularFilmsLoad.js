98b9fd189a5a4869bbc54b744cb944b7742c0d6b
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-11-19 17:05:07 +0200

    added func to markUp films

--- src/js/popularFilmsLoad.js
+++ src/js/popularFilmsLoad.js
@@ -1,7 +1,19 @@
-import {
-  fetchTopWeekMovie,
-  fetchMovieByKeyWord,
-  fetchMovieFullInfo,
-} from './apiService';
+// import {
+//   fetchTopWeekMovie,
+//   fetchMovieByKeyWord,
+//   fetchMovieFullInfo,
+// } from './apiService';
+import ApiService from './apiService';
 
-fetchTopWeekMovie();
+const refs = {
+  //Сюда поставить контейнер для фильмов
+  filmContainer: document.querySelector('filmContainerTest'),
+};
+const filmsApiService = new ApiService();
+
+function markupFilms(films) {
+  //вместо решетки карточку фильмов
+  refs.filmContainer.insertAdjacentHTML('beforeend', filmContainerTest(films));
+}
+
+// filmsApiService.fetchMovie();
