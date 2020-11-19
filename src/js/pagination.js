54417d8955bd0227c06eb6643a07f2fd00f9bcb7
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-11-19 15:07:34 +0200

    pagination first steps

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,10 +1,48 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
-const pagination = new Pagination('pagination');
+const API_KEY = '3f80d4cf4eb52d6e9d2ef400ea3d2acb';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-const container = document.getElementById('pagination');
-const pagination = new Pagination(container);
+let pageMarkup = '';
+
+const paginationEl = document.querySelector('#pagination');
+
+// const pagination = new Pagination('pagination');
+
+// const container = document.getElementById('pagination');
+// const pagination = new Pagination(container);
+
+paginationEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  const paginationBtnList = document.querySelectorAll('button.btn-number');
+  const lastPage = Number(paginationBtnList[paginationBtnList - 1].textContent);
+
+  if (Number(event.target.textContent)) {
+    onNumberBtnClick(event);
+  } else if (event.target.textContent === '→' && currentPage < lastPage - 1) {
+    onRightBtnClick();
+  } else if (event.target.textContent === '←' && currentPage > 0) {
+    onLeftBtnClick();
+  } else {
+    return;
+  }
+
+  clearMarkup();
+  clearContainer();
+}
+
+function fetchPage(page) {
+  return fetch(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`,
+  );
+}
+
+function paginationMarkup(length) {
+  renderMarkup(length);
+  paginationEl.insertAdjacentElement('beforeend', pageMarkup);
+}
 
 const options = {
   totalItems: 500,
@@ -33,4 +71,68 @@ const options = {
   },
 };
 
-const pagination = new Pagination('pagination', options);
+function renderMarkup(length) {
+  if (currentPage + 1 < 5) {
+    pageMarkup =
+      '<li class="pagination-item"><button class="#">&#8592</button></li>';
+
+    for (let i = 1; i <= 5; i += 1) {
+      const pageItem = `<li class="pagination-item"><button class="btn-number">${i}</button></li>`;
+      pageMarkup += pageItem;
+    }
+    pageMarkup += `<li class="pagination-item"><button class="#">...</button></li>
+  <li class="pagination-item"><button class="btn-number">${length}</button></li>
+  <li class="pagination-item"><button class="#">&#8594</button></li>`;
+  } else if (currentPage + 1 >= 5 && currentPage + 1 < length - 3) {
+    pageMarkup = `<li class="pagination-item"><button class="btn-number">1</button></li>
+    <li class='pagination-item'><button class="#">...</button></li>
+    <li class='pagination-item'><button class="btn-number">${
+      currentPage - 1
+    }</button></li>
+    <li class='pagination-item'><button class="btn-number">${currentPage}</button></li>
+    <li class='pagination-item'><button class="btn-number">${
+      currentPage + 1
+    }</button></li>
+    <li class='pagination-item'><button class="btn-number">${
+      currentPage + 2
+    }</button></li>
+    <li class='pagination-item'><button class="btn-number">${
+      currentPage + 3
+    }</button></li>
+    <li class='pagination-item'><button class="#">...</button></li>
+    <li class="pagination-item"><button class="btn-number">${length}</button></li>
+    <li class="pagination-item"><button class="#">&#8594</button></li>`;
+  } else {
+    pageMarkup = `<li class="pagination-item"><button class="#">&#8592</button></li>
+  <li class='pagination-item'><button class="btn-number">1</button></li>
+      <li class='pagination-item'><button class="#">...</button></li>
+      <li class='pagination-item'><button class="btn-number">${
+        length - 4
+      }</button></li>
+      <li class='pagination-item'><button class="btn-number">${
+        length - 3
+      }</button></li>
+      <li class='pagination-item'><button class="btn-number">${
+        length - 2
+      }</button></li>
+      <li class='pagination-item'><button class="btn-number">${
+        length - 1
+      }</button></li>
+      <li class='pagination-item'><button class="btn-number">${length}</button></li>
+      <li class="pagination-item"><button class="#">&#8594</button></li>`;
+  }
+}
+
+function clearMarkup() {
+  paginationEl.innerHTML = '';
+}
+
+function onRightBtnClick() {
+  currentPage += 1;
+}
+
+function onLeftBtnClick() {
+  currentPage -= 1;
+}
+
+// const pagination = new Pagination('pagination', options);
