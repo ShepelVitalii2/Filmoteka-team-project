aa4dd713267dc0eba196ec07dfdad91aeb379f35
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2020-11-22 13:41:49 +0200

    Смена активных кнопок в библ

--- src/js/activeBtn.js
+++ src/js/activeBtn.js
@@ -1,8 +1,25 @@
-const btn1 = document.querySelector('.btnWatched');
-const btn2 = document.querySelector('.btnQueue');
+const btnWatched = document.querySelector('.btnWatched');
+const btnQueue = document.querySelector('.btnQueue');
 
-const btnList = document.querySelector('.button-list');
+// const btnList = document.querySelector('.button-list');
 
-// btnList.addEventListener.add('click', onClick);
+btnQueue.addEventListener('click',addActive);
+btnWatched.addEventListener('click', onActive);
 
+
+function addActive() {
+    btnQueue.classList.remove('lib-btn-off');
+   btnQueue.classList.add('lib-btn-active');
+
+    btnWatched.classList.remove('lib-btn-active');
+   btnWatched.classList.add('lib-btn-off');
+}
+
+function onActive() {
+    btnWatched.classList.add('lib-btn-active');
+    btnWatched.classList.remove('lib-btn-off');
+
+    btnQueue.classList.add('lib-btn-off');
+    btnQueue.classList.remove('lib-btn-active');
+}
 
